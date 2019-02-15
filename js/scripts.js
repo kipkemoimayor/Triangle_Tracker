4ddd56135455603990aa1e8a57f739d9af0ffdd4
kipkemoimayor <collinskipkemoi24@gmail.com>
2019-02-15 12:52:24 +0300

    this commit added a script to check if input gives a triangle

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,8 @@
-function triangles(x,y,z){
-  
+function triangles(){
+  var length_x=parseInt(document.getElementById("one").value);
+  var length_y = parseInt(document.getElementById("two").value);
+  var length_z= parseInt(document.getElementById("three").value);
+  if(((length_x+length_y)>length_z) && ((length_x+length_z)>length_y) && ((length_y+length_z)>length_x)){
+    document.getElementById("txtresult").innerHTML= 'Its a Triangle';
+  }
 }
