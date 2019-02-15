c5034f89dfc0c3f2351e194cec551517fcd85e34
kipkemoimayor <collinskipkemoi24@gmail.com>
2019-02-15 14:30:11 +0300

    this commit is to add an array to store input values and also put a condition to check wheter is an equiletaral triagle

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,45 @@
 function triangles(){
-  var length_x=parseInt(document.getElementById("one").value);
+  var length_x = parseInt(document.getElementById("one").value);
   var length_y = parseInt(document.getElementById("two").value);
-  var length_z= parseInt(document.getElementById("three").value);
+  var length_z = parseInt(document.getElementById("three").value);
+
+  var emptArr=[];
+  emptArr.push(length_x,length_y,length_z)
+  var randomWords=["Triangle of your own make","elephant muscle size","antelope horn length","score margin on which chelsea lost by","numberof lung a person has"];
+  var accesArr = Math.floor(Math.random()*randomWords.length)
   if(((length_x+length_y)>length_z) && ((length_x+length_z)>length_y) && ((length_y+length_z)>length_x)){
-    document.getElementById("txtresult").innerHTML= 'Its a Triangle';
+    alert('Its a Triangle');
+
+    if((length_x==length_y  && length_x!=length_z)||(length_x==length_z &&length_x!=length_y) ){
+      document.getElementById("txtresult").innerHTML= 'It is an isoceles Triangle';
+    }
+    else if((length_y==length_x && length_x!=length_z)||(length_y==length_z && length_x!=length_y)){
+      document.getElementById("txtresult").innerHTML= 'It is an isoceles Triangle';
+    }
+    else if((length_z==length_y && length_z!=length_x)||(length_z==length_x && length_z!=length_y)){
+      document.getElementById("txtresult").innerHTML= 'It is an isoceles Triangle';
+
+    }
+    else if(length_x==length_y && length_x==length_z && length_y==length_z){
+      document.getElementById("txtresult").innerHTML= 'It is an Equilateral Triangle';
+
+    }
+    else if(length_x!=length_y && length_x!=length_z && length_z!=length_y ){
+      document.getElementById("txtresult").innerHTML= 'It is an Scalene Triangle';
+    }
   }
+  else{
+    alert("Its not a Triangle maybe its a "+randomWords[accesArr]);
+    document.getElementById("txtresult").innerHTML= 'Its not in anyway a Triangle'+' Your values are '+emptArr;
+    return false;
+  }
+
+  alert("your values are "+emptArr)
+	var output=0;
+	for(i=0;i<emptArr.length;i++){
+		output+=emptArr[i];
+
+    }
+	alert("Perimeter is "+output)
+
 }
