62859377324ef292cb658044f81e57a9dc9123f9
kipkemoimayor <collinskipkemoi24@gmail.com>
2019-02-16 17:59:45 +0300

    This commit is to add a javascript function to draw a rectangle on button clicked

--- js/scripts.js
+++ js/scripts.js
@@ -12,20 +12,89 @@ function triangles(){
 
     if((length_x==length_y  && length_x!=length_z)||(length_x==length_z &&length_x!=length_y) ){
       document.getElementById("txtresult").innerHTML= 'It is an isoceles Triangle';
+      document.getElementById("main2").innerHTML= 'isoceles Triangle';
+      var canvas=document.getElementById("main");
+      if(canvas.getContext){
+
+        var ctx=canvas.getContext('2d')
+        var s=canvas.width;
+        var h=canvas.height;
+
+        var path=new Path2D();
+        path.moveTo((s/2)+120,h/2);
+        path.lineTo((s/2),(h/2)-70);
+        path.lineTo((s/2),h/2);
+        ctx.fill(path);
+    
+      }
     }
     else if((length_y==length_x && length_x!=length_z)||(length_y==length_z && length_x!=length_y)){
       document.getElementById("txtresult").innerHTML= 'It is an isoceles Triangle';
+      document.getElementById("main2").innerHTML= 'isoceles Triangle';
+      var canvas=document.getElementById("main");
+      if(canvas.getContext){
+
+        var ctx=canvas.getContext('2d')
+        var s=canvas.width;
+        var h=canvas.height;
+
+        var path=new Path2D();
+        path.moveTo((s/2)+120,h/2);
+        path.lineTo((s/2),(h/2)-70);
+        path.lineTo((s/2),h/2);
+        ctx.fill(path);
+      }
     }
     else if((length_z==length_y && length_z!=length_x)||(length_z==length_x && length_z!=length_y)){
       document.getElementById("txtresult").innerHTML= 'It is an isoceles Triangle';
+      document.getElementById("main2").innerHTML= 'isoceles Triangle';
+      var canvas=document.getElementById("main");
+      if(canvas.getContext){
+
+        var ctx=canvas.getContext('2d')
+        var s=canvas.width;
+        var h=canvas.height;
+
+        var path=new Path2D();
+        path.moveTo((s/2)+120,h/2);
+        path.lineTo((s/2),(h/2)-70);
+        path.lineTo((s/2),h/2);
+        ctx.fill(path);
+      }
 
     }
     else if(length_x==length_y && length_x==length_z && length_y==length_z){
       document.getElementById("txtresult").innerHTML= 'It is an Equilateral Triangle';
+      document.getElementById("main2").innerHTML= 'Equilateral Triangle';
+      var canvas=document.getElementById("main");
+      if(canvas.getContext){
+        var ctx=canvas.getContext('2d')
+        var s=canvas.width;
+        var h=canvas.height;
+        var path=new Path2D();
+        path.moveTo((s/2)+100,h/2);
+        path.lineTo((s/2),(h/2)-70);
+        path.lineTo((s/2)-90,h/2);
+        ctx.fill(path);
+
+      }
 
     }
     else if(length_x!=length_y && length_x!=length_z && length_z!=length_y ){
       document.getElementById("txtresult").innerHTML= 'It is an Scalene Triangle';
+      document.getElementById("main2").innerHTML= 'Scalene Triangle';
+      var canvas=document.getElementById("main");
+      if(canvas.getContext){
+        var ctx=canvas.getContext('2d')
+        var s=canvas.width;
+        var h=canvas.height;
+        var path=new Path2D();
+        path.moveTo((s/2)+50,h/2);
+        path.lineTo((s/2),(h/2)-70);
+        path.lineTo((s/2)-120,h/2);
+        ctx.fill(path);
+
+      }
     }
   }
   else{
@@ -77,8 +146,6 @@ function dra(){
   }
 }
 dra();
-
-draw();
 //Draws Scalene
 function dr(){
   var canvas=document.getElementById("scalene");
@@ -91,7 +158,7 @@ function dr(){
     path.lineTo((s/2),(h/2)-70);
     path.lineTo((s/2)-120,h/2);
     ctx.fill(path);
-    ctx.fillStyle="rgba(250,255,255,0.9)";
+    ctx.fillStyle="rgb(250,255,255,0.9)";
   }
 }
 dr();
